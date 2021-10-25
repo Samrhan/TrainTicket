183afdf6a87825b5e8ce961954ed3144de2916af
Samrhan <samuel.bader@efrei.net>
2021-10-25 14:07:57 +0200

    edit user table

--- back/sql/create.sql
+++ back/sql/create.sql
@@ -5,8 +5,9 @@ CREATE TABLE user
     id        INT AUTO_INCREMENT
         PRIMARY KEY,
     mail      VARCHAR(200) NOT NULL,
+    lastname  VARCHAR(20)  NOT NULL,
+    firstname VARCHAR(20)  NOT NULL,
     password  VARCHAR(200) NOT NULL,
-    birthdate DATE         NOT NULL,
+    birthdate VARCHAR(10)  NOT NULL,
     address   TEXT         NOT NULL
 );
-
